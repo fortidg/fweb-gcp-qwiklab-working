e0618e475ae2fcb89ed5e45d311698b58ef43265
fortidg <dgooch@fortinet.com>
2023-08-02 14:53:06 +0000

    first commit

---
+++ fwebvm-juice/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {}
+variable "name" {}
+variable "region" {}
+variable "zone" {}
+variable "machine" {}
+variable "image" {}
+variable "license_file" {}
+variable "ubuntu_image" {}
+# vpc module
+variable "vpcs" {}
+# subnet module
+variable "subnets" {}
+variable "subnet_cidrs" {}
+#####
+variable "juice_shop_image_name" {}
+variable "juice_shop_machine" {}
