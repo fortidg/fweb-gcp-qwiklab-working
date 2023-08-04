73caf6d8ef1c6c37caecc8052f31136acc1a5ef9
fortidg <dgooch@fortinet.com>
2023-08-04 20:36:35 +0000

    cha cha cha changes

--- fweb-juice/dns.tf
+++ fweb-juice/dns.tf
@@ -14,25 +14,3 @@ resource "google_dns_record_set" "a-record" {
   ]
 }
 
-/* resource "google_dns_record_set" "ns-record" {
-  name         = google_dns_managed_zone.default.dns_name
-  managed_zone = google_dns_managed_zone.default.name
-  type         = "NS"
-  ttl          = 21600
-  rrdatas = [
-   "ns-cloud-c1.googledomains.com.",
-   "ns-cloud-c2.googledomains.com.",
-   "ns-cloud-c3.googledomains.com.",
-   "ns-cloud-c4.googledomains.com.",
-  ]
-}
-
-resource "google_dns_record_set" "soa-record" {
-  name         = google_dns_managed_zone.default.dns_name
-  managed_zone = google_dns_managed_zone.default.name
-  type         = "SOA"
-  ttl          = 21600
-  rrdatas = [
-   "ns-cloud-c1.googledomains.com. cloud-dns-hostmaster.google.com. 1 21600 3600 259200 300"
-  ]
-} */
--- fweb-juice/main.tf
+++ fweb-juice/main.tf
@@ -134,18 +134,3 @@ resource "google_compute_instance" "juice_shop" {
   }
 }
 
- /* resource "google_dns_managed_zone" "default" {
-   name          = "${module.random.random_string}-fwebtraincse-zone"
-   dns_name      = "${module.random.random_string}.fwebtraincse.com."
-   description   = "fortiweb public dns zone"
- }
-
-resource "google_dns_record_set" "default" {
-  name         = google_dns_managed_zone.default.dns_name
-  managed_zone = google_dns_managed_zone.default.name
-  type         = "A"
-  ttl          = 300
-  rrdatas = [
-    module.instances.fweb_ip
-  ]
-} */
