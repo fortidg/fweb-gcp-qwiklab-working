b04b6a918533468b9454a0b9917772a5894045d9
fortidg <dgooch@fortinet.com>
2023-08-04 17:44:20 +0000

    updating dns

--- fweb-juice/dns.tf
+++ fweb-juice/dns.tf
@@ -4,12 +4,25 @@
    description   = "fortiweb public dns zone"
  }
 
-resource "google_dns_record_set" "default" {
+resource "google_dns_record_set" "a-record" {
   name         = google_dns_managed_zone.default.dns_name
   managed_zone = google_dns_managed_zone.default.name
   type         = "A"
   ttl          = 300
   rrdatas = [
     module.instances.fweb_ip
+  ]
+}
+
+resource "google_dns_record_set" "ns-record" {
+  name         = google_dns_managed_zone.default.dns_name
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "NS"
+  ttl          = 300
+  rrdatas = [
+   "ns-cloud-c1.googledomains.com.",
+   "ns-cloud-c2.googledomains.com.",
+   "ns-cloud-c3.googledomains.com.",
+   "ns-cloud-c4.googledomains.com.",
   ]
 }
